becf4f45c54b12929d713226028c451568a696de
Giridhar Rajkumar <vgrk.2017@gmail.com>
2019-06-13 22:23:31 +0100

    Added code snippet for beforeEach()

--- test/chapterFourFive.js
+++ test/chapterFourFive.js
@@ -3,6 +3,17 @@
 
 var assert = require('assert');
 
+/* describe("Mathematical Operations - Test Suite", function(){
+
+  beforeEach(function(done){
+
+  this.timeout(500);
+
+  setTimeout(done,3000);
+
+}); */
+
+
 describe('Mathematical Operations - Test Suite', function(){
 
   this.timeout(10000);
